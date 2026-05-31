301191105ba2ae73b9e9ab10c1f04b655c37001d
JustASimpleMahdi <just.a.simple.mahdi@gmail.com>
2026-05-31 16:33:57 +0330

    fix DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        (new SettingsSeeder())->run();
+        (new ManagerSeeder())->run();
         (new ProductSeeder())->run();
     }
 }
